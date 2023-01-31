ce27651a03ea493eb4e70be3f7fd0648f64ff3ae
Stanislav Khrapov <stanislav.khrapov@chintai.io>
2023-01-31 17:07:38 +0100

    document random

--- src/order_matching/random.py
+++ src/order_matching/random.py
@@ -3,10 +3,32 @@ from numpy.random import Generator, default_rng
 
 
 def get_random_generator(seed: int = None) -> Generator:
+    """Get numpy random number generator.
+
+    Parameters
+    ----------
+    seed
+        Random seed
+
+    Returns
+    -------
+    Generator
+    """
     return default_rng(seed=seed)
 
 
 def get_faker(seed: int = None) -> Faker:
+    """Get Faker random generator.
+
+    Parameters
+    ----------
+    seed
+        Random seed
+
+    Returns
+    -------
+    Faker
+    """
     faker = Faker()
     faker.seed_instance(seed)
     return faker
