5eed11d7de260f16df73ee8bf59dd772ee273dca
Stanislav Khrapov <stanislav.khrapov@chintai.io>
2023-02-02 06:24:27 +0100

    add bugbear

--- src/order_matching/matching_engine.py
+++ src/order_matching/matching_engine.py
@@ -56,7 +56,7 @@ trade_id='c4da537c-1651-4dae-8486-7db30d67b366', timestamp=Timestamp('2023-01-02
             Executed trades storage object
         """
         self._timestamp = timestamp
-        self._queue += orders if orders is not None else Orders()
+        self._queue += orders if orders else Orders()
         self._queue += self._get_expired_orders()
         trades = ExecutedTrades()
         while not self._queue.is_empty:
